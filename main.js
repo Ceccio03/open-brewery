389e00fe9f5ca0629f8c0d9366cbd29a2b4ad519
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-13 13:25:26 +0200

    commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-DBService.getBreweries().then(birrerie => console.log(birrerie));
+let pageNumber = 1;
+
+DBService.getBreweries(pageNumber).then(breweries => console.log(breweries));
+
+function render(breweries) {
+    for (const brewery of breweries) {
+        console.log(brewery.name);
+    }
+}
+
+function previous() {
+    pageNumber--;
+    DBService.getBreweries(pageNumber).then(breweries => console.log(breweries));
+}
+
+function next() {
+    pageNumber++;
+    DBService.getBreweries(pageNumber).then(breweries => console.log(breweries));
+}
--- services/db-service.js
+++ services/db-service.js
@@ -1,6 +1,8 @@
 class DBService {
-    static getBreweries() {
-        const url = 'https://api.openbrewerydb.org/v1/breweries';
+    static getBreweries(pageNumber) {
+        const url = 'https://api.openbrewerydb.org/v1/breweries?per_page=108page=' + pageNumber;
+
+        console.log(url);
 
         return fetch(url).then(resp => resp.json());
     }
