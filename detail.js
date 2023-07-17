53e05481d61f658793da344b82c940fcbd2a2ae6
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-17 10:28:02 +0200

    commit

--- detail.js
+++ detail.js
@@ -5,6 +5,13 @@ if (dataString) {
     console.log(data);
 }
 
-function render() {
-    
+function render(brewery) {
+    const breweryContainer = document.getElementById('brewery-container');
+
+    breweryContainer.appendChild(createElementWithString('span', brewery.name));
+    breweryContainer.appendChild(createElementWithString('span', brewery.city));
+    breweryContainer.appendChild(createElementWithString('span', brewery.state));
+    breweryContainer.appendChild(createElementWithString('span', brewery.address));
+    breweryContainer.appendChild(createElementWithString('span', brewery.type));
+    breweryContainer.appendChild(createElementWithString('span', brewery.website.url));
 }
